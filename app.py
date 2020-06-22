1929ff967a0a0312d5daade8f5888deecced233c
onshimiye <onshimiye17@gmail.com>
2020-06-22 15:24:23 +0200

    testing json endpoint

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 CORS(app)
 
 
 @app.route("/")
 def hello():
-    return "hello world"
+    return jsonify('Cannot GET /')
+
+
+@app.route("/json")
+def json_data():
+    data = [
+        {
+            'fullname': 'Olivier Nshimiye',
+            'username': 'onshimiye',
+            'email': 'onshimiye17@gmail.com',
+            'points': '5',
+        },
+        {
+            'fullname': 'Mark Essien',
+            'username': 'mark',
+            'email': 'markessien@hotels.ng',
+            'points': '10',
+        }
+    ]
+    return jsonify(data)
+
+
+@app.route("/csv")
+def csv_file():
+    return send_from_directory('.', 'data.csv')
 
 
 if __name__ == '__main__':
